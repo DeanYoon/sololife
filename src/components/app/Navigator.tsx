c2411863809dc8612ba6b13407cfc1d89dc786fa
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-07-02 17:58:53 +0900

    navigator modified

--- src/components/app/Navigator.tsx
+++ src/components/app/Navigator.tsx
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import SmartToysIcon from "@mui/icons-material/SmartToyOutlined";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Wrapper = styled.div`
   width: 500px;
@@ -7,7 +14,7 @@ const Wrapper = styled.div`
   position: fixed;
   bottom: 0;
   background-color: inherit;
-  box-shadow: 0px -1px 3px rgba(50, 50, 50, 0.2);
+  box-shadow: 0px -1px 3px rgba(11, 3, 3, 0.2);
 `;
 
 const ButtonBox = styled.a`
@@ -17,35 +24,46 @@ const ButtonBox = styled.a`
   justify-content: center;
   align-items: center;
 `;
-const ButtonIcon = styled.img`
-  width: 40px;
-`;
+
 const ButtonText = styled.p`
   font-weight: 400;
   margin-top: 10px;
 `;
 
 function Navigator() {
+  const location = useLocation();
+  const [currentUrl, setCurrentUrl] = useState("");
+
+  useEffect(() => {
+    setCurrentUrl(location.pathname);
+  }, [location]);
+
   return (
     <Wrapper>
       <ButtonBox href="/home">
-        <ButtonIcon src="./imgs/NavBar/home.png" />
+        <HomeOutlinedIcon
+          fontSize="large"
+          style={{ color: currentUrl === "/home" ? "tomato" : "" }}
+        />
         <ButtonText>홈</ButtonText>
       </ButtonBox>
       <ButtonBox href="/">
-        <ButtonIcon src="./imgs/NavBar/pen.png" />
+        <EditOutlinedIcon fontSize="large" />
         <ButtonText>글쓰기</ButtonText>
       </ButtonBox>
       <ButtonBox href="/">
-        <ButtonIcon src="./imgs/NavBar/bot.png" />
+        <SmartToysIcon fontSize="large" />
         <ButtonText>AI 친구</ButtonText>
       </ButtonBox>
       <ButtonBox href="/">
-        <ButtonIcon src="./imgs/NavBar/manager.png" />
+        <SupportAgentIcon fontSize="large" />
         <ButtonText>전문 매니저</ButtonText>
       </ButtonBox>
       <ButtonBox href="/myprofile">
-        <ButtonIcon src="./imgs/NavBar/profile.png" />
+        <PersonOutlinedIcon
+          fontSize="large"
+          style={{ color: currentUrl === "/myprofile" ? "tomato" : "" }}
+        />
         <ButtonText>마이페이지</ButtonText>
       </ButtonBox>
     </Wrapper>
