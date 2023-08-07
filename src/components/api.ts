90a47f5dd262a5ab42b55c2903645237df72ce0d
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-08-07 19:16:16 +0900

    api modified to robertchoi domain

--- src/components/api.ts
+++ src/components/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // export const DOMAIN_URL = "http://localhost:3001";
 export const DOMAIN_URL =
-  "https://port-0-sololife-node-backend-rt92alkd9190z.sel4.cloudtype.app";
+  "https://port-0-sololife-node-backend-r8xoo2mledsvukh.sel3.cloudtype.app";
 export const POSTS_API = `${DOMAIN_URL}/api/posts`;
 export const USERS_API = `${DOMAIN_URL}/api/users`;
 export const postInsertUserData = async (data: object) => {
